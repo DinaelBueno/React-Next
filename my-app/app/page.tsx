7a1535897ef2c55f09df5aecf5a1b99dea235472
DinaelBueno <dinael.bueno.rosa@gmail.com>
2026-06-16 11:51:05 -0300

    Add files via upload

---
+++ my-app/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+
+  return (
+    <div className = "min-h-screen">
+      <main>
+        <nav className="bg-background border-b-4 border-foreground/12 px-6 py-10 flex items-center justify-between">
+          <h1 className="text-2xl">❄ ESFRIAR ❄</h1>
+
+        </nav>
+      </main>
+    </div>
+    
+  )
+
+  
+
+}
